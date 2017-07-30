b4bc1777069735a4333118ee4df4a5ebc005a522
annie ke <annieke8@gmail.com>
2017-07-29 23:56:59 -0400

    finished candles without number of candles variable

---
+++ candles-ke.R
@@ -0,0 +1,27 @@
+# This function takes an input n, and simulates the first attempt to blow out n candles
+blow_n_candles_once = function(n) {
+  return(sample(1:n, 1))
+}
+
+# This function takes no inputs and returns the number of attempts to blow out 35 candles
+# assuming that each blow extinguishes between 1 to all the remaining candles
+blow_out_candles = function() {
+  a = 0
+  n = 35
+  while (n > 0) {
+    n = n - blow_n_candles_once(n)
+    a = a + 1
+  }
+  return(a)
+}
+
+blow_and_plot = function(num_repetitions) {
+  all_experiments = replicate(num_repetitions, blow_out_candles())
+  
+  all_experiments_table = table(all_experiments)
+  
+  barplot(all_experiments_table)
+  
+  return(all_experiments_table)
+}
+
